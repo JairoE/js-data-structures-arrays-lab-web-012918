4eef0a6a44772f88ff67ea7fa7933c24f544d9db
Jairo Espinosa <jairoespinosa95@gmail.com>
2018-03-12 03:09:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 // Write your solution here!
 const drivers = ["Milo", "Otis", "Garfield"];
 
-function destructivelyAppendDriver() {
-  drivers.unshift("Bob")
+function destructivelyAppendDriver(name) {
+  drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift();
+}
+
+function appendDriver(name) {
+  let newAr = drivers.slice();
+  newAr.push(name);
 }
